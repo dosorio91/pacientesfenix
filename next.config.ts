309a49a2dbee85d9a5ff7ac31ba185ae711ffdec
Daniel Andrés Osorio Alvarado <daniel@mistatas.com>
2025-09-03 17:07:02 -0400

    Configuración para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/pacientesfenix',
+  assetPrefix: '/pacientesfenix/',
 };
 
 export default nextConfig;
